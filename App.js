a1a192c94b7f0ad67b1c9cafda53bdb6eb0c08c0
Nahuel Sangiacomo <psangiacomo@adifse.com.ar>
2021-12-17 11:49:56 -0300

    Sangiacomo Pablo Nahuel - Desafío: Creación aplicación Expo

---
+++ App.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {
+  Colors,
+  Header,
+} from 'react-native/Libraries/NewAppScreen';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
+
+import type {Node} from 'react';
+import React from 'react';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text
+            style={[
+              styles.sectionTitle,
+              {
+                color: isDarkMode ? Colors.white : Colors.black,
+              },
+            ]}>Hola, Coder!
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default App;
